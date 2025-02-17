ff939a1f3144aca3299c1c4f1ede7a0432b3e227
Oleh <118124646+Olehq312@users.noreply.github.com>
2025-02-17 12:34:46 +0100

    9 videos done

---
+++ src/modules/useProducts.ts
@@ -0,0 +1,35 @@
+import {ref} from 'vue';
+import type {Product} from '../interfaces/interfaces';
+
+
+export const useProducts = () => {
+    const error = ref<string | null>(null); 
+    const loading = ref<boolean>(false);
+    const products = ref<Product[]>([]);
+
+    const fetchProducts = async (): Promise<void> => {
+        loading.value = true;
+        try {
+            const response = await fetch('https://ments-restapi.onrender.com/api/products');
+            if (!response.ok) {
+                throw new Error('Nodata available');
+            }
+
+            const data: Product[] = await response.json();
+            products.value = data;
+            console.log("products fetched", products.value);
+        } 
+        catch (err) {
+            error.value = (err as Error).message;
+        }
+        finally {
+            loading.value = false;
+        }
+    };
+    return {
+        error,
+         loading, 
+         products, 
+         fetchProducts
+        };
+};
