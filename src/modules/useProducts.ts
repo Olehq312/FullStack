3d326e2d9b14c595e8a4215b0731fa9ca5291760
Oleh <118124646+Olehq312@users.noreply.github.com>
2025-02-22 09:38:34 +0100

    21 videos done

--- src/modules/useProducts.ts
+++ src/modules/useProducts.ts
@@ -26,10 +26,91 @@ export const useProducts = () => {
             loading.value = false;
         }
     };
+
+
+    const addProduct = async (): Promise<void> => {
+        try {
+            const token = localStorage.getItem('lsToken');
+            const userID = localStorage.getItem('userIDToken');
+            if (!token) {
+                throw new Error('No token available');
+            }
+            if (!userID) {
+                throw new Error('No userID available');
+            }
+
+            const response = await fetch('https://ments-restapi.onrender.com/api/products', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'auth-token': token
+                },
+                body: JSON.stringify({
+                    name: 'Product from frontend',
+                    description: 'Product description',
+                    imageURL: 'https://picsum.photos/500/500',
+                    price: 100,
+                    stock: 45,
+                    discount: false,
+                    discountPct: 0,
+                    isHidden: false,
+                    _createdBy: userID
+                })
+            })
+            if (!response.ok) {
+                const errorResponse = await response.json()
+                throw new Error(errorResponse.error || 'An error occurred');
+
+            }
+            
+            const newProduct: Product = await response.json();
+            products.value.push(newProduct);
+            console.log('product added', newProduct);
+            await fetchProducts();
+
+        }
+        catch (err) {
+            error.value = (err as Error).message;
+        }
+    }
+
+
+
+
+    const deleteProduct = async (id: string): Promise<void> => {
+        try {
+            const token = localStorage.getItem('lsToken');
+
+            if (!token) {
+                throw new Error('No token available');
+            }
+
+            console.log('id test', id);
+            const response = await fetch(`https://ments-restapi.onrender.com/api/products/${id}`, {
+                method: 'DELETE',
+                headers: {
+                    'auth-token': token
+                },
+            })
+
+            if (!response.ok) {
+                throw new Error('An error occurred');
+            }
+
+            products.value = products.value.filter(product => product._id !== id);
+            console.log('product deleted', id);
+        }
+
+        catch (err) {
+            error.value = (err as Error).message;
+        }
+    };
     return {
         error,
          loading, 
          products, 
-         fetchProducts
+         fetchProducts,
+         deleteProduct,
+         addProduct
         };
 };
--- src/router/index.ts
+++ src/router/index.ts
@@ -19,7 +19,17 @@ const router = createRouter({
       name: 'products',
       component: () => import('../views/ProductsView.vue'),
     },
+    {
+      path: '/auth',
+      name: 'auth',
+      component: () => import('../views/admin/AuthView.vue'),
+    },
 
+    {
+      path: '/admin',
+      name: 'admin',
+      component: () => import('../views/admin/AdminView.vue'),
+    },
   ],
 })
 
